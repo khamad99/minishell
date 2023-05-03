1041490f3cef36b85a79e6d327011fabc70060c4
kalshaer <kalshaer@DESKTOP-JF1BPMD.localdomain>
2023-05-03 21:10:09 +0400

    1

---
+++ parsing/parse.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ooutabac <ooutabac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 11:29:43 by ooutabac          #+#    #+#             */
+/*   Updated: 2023/03/05 14:20:01 by ooutabac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+t_shell_s	*parse(char *str, char **envp)
+{
+	t_shell_s	*minishell;
+
+	minishell = ft_calloc(sizeof(t_shell_s), 1);
+	minishell->cmd_line = ft_strdup(str);
+	minishell = get_path(minishell, envp);
+	minishell = get_env_struct(minishell, envp);
+	minishell = lexer(minishell, str);
+	minishell = raw_lexer(minishell, str);
+	if (minishell && minishell->lexer)
+		minishell->lexer->command_blocks = split_pipes(str);
+	minishell = dollar_sign(minishell);
+	if (check_validity(minishell, str) == FALSE)
+	{
+		free_everything(minishell);
+		return (NULL);
+	}
+	if (minishell)
+		minishell->num_pipes = count_pipes(str);
+	minishell = get_num_commands(minishell);
+	minishell = get_commands(minishell);
+	minishell = get_flags(minishell);
+	minishell = get_execution_blocks(minishell);
+	return (minishell);
+}
---
+++ src/main.c
@@ -0,0 +1,75 @@
+#include "../include/minishell.h"
+
+void ft_ctrl_c()
+{
+	rl_replace_line("", 0);
+	printf("\n");
+	rl_on_new_line();
+	rl_redisplay();
+}
+
+void ft_signal()
+{
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, ft_ctrl_c);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	t_shell_s	*minishell;
+	(void)argc;
+	(void)argv;
+	char *cmd;
+	int pid;
+	char **av;
+	int flag;
+
+	while (1)
+	{
+		flag = 0;
+		ft_signal();
+		cmd = readline("minishell🤓$ ");
+		minishell = parse(cmd, envp);
+		// printf("%s\n", minishell->lexer->tokens[0]);
+		if (cmd == NULL)
+		{
+			free_everything(minishell);
+			free(cmd);
+			printf("\n");
+			exit (0);
+		}
+		av = ft_split(cmd, ' ');
+		if(!cmd)
+            exit(EXIT_SUCCESS);
+		if (strlen(cmd) > 0)
+      		add_history(cmd);
+        if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
+        {
+            free(cmd);
+            continue;
+        }
+        if(strcmp(cmd, "exit") == 0)
+        {
+            free(cmd);
+            break;
+        }
+		cmd = ft_strjoin("/bin/", av[0]);
+		if (access(cmd, 0) == 0)
+		{
+			flag = 1;
+		}
+		if(flag)
+		{
+		pid = fork();
+		if(pid == 0)
+			execve(cmd, av, envp);
+		}
+		waitpid(pid, &flag, 0);
+        free(cmd);
+		free_2d(av);
+		free_everything(minishell);
+		cmd = NULL;
+    }
+
+    exit(EXIT_SUCCESS);
+}
